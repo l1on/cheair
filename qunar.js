a6bba7b1ffda2038307d82b6e5a8f4d0bbedb389
l1on <iluvzhouying@gmail.com>
2015-10-01 01:45:55 +0800

    fixed the shit

--- qunar.js
+++ qunar.js
@@ -1,13 +1,22 @@
-var SELECTORS = {
+var SEL = {
 	FLIGHT_LEGS: '.b_spc_list',
-	LOWEST_PRICE: '.low_pr'
-
+	LOWEST_PRICE: '.low_pr',
+	FULL_PRICE_BUTTON: '#filter_showAllprice_yes',
+	BOOKING_BUTTON: '.btn_book',
+	PRICE_LOADING_PROGRESS_BAR: '.m-loader-inner[style*=overflow]'
 };
+
+var EVENTS_TO_SCREEN_CAPTURE = [
+	'qunar.loaded', 
+	'qunar.prices.loaded',
+	'qunar.button.fullPrice.loaded',
+	'qunar.prices.vendors.loaded'
+];
 
 var casper = require('casper').create({
 	verbose: true,
     logLevel: "debug",
-    waitTimeout: 1000 * 40,
+    waitTimeout: 1000 * 50,
 	pageSettings: {
 		userAgent: 'Chrome/44',
         loadImages: false,        
@@ -17,46 +26,43 @@ var casper = require('casper').create({
 
 casper.on('waitFor.timeout', function(timeout, details) {
 	var incident = details.selector ? details.selector : details.testFx.name;
-	this.capture('screenshots/qunar_timeout_on_' + incident + '.png');
+	this.capture('screenshots/qunar.timeout.' + incident + '.png');
+});
+
+casper.each(EVENTS_TO_SCREEN_CAPTURE, function(self, event) {
+	self.on(event, function() {
+		this.capture('screenshots/' + event + '.png');
+	});
 });
 
 casper.on('qunar.loaded', function() {
-	this.capture('screenshots/qunar_loaded.png');
-
-	casper.waitWhileSelector('.m-loader-inner[style*=overflow]', function then() {
+	this.waitWhileSelector(SEL.PRICE_LOADING_PROGRESS_BAR, function then() {
 		this.emit('qunar.prices.loaded');
 	});
 });
 
-casper.on('qunar.prices.loaded', function() {
-	this.capture('screenshots/qunar_prices_loaded.png');
-	
-	this.waitUntilVisible('#filter_showAllprice_yes', function then() {
-		this.emit('qunar.button.totalPrice.loaded');
-
-	    this.click('#filter_showAllprice_yes');    
-	    this.click('.btn_book');
-	    this.emit('qunar.button.booking.first.clicked');		
+casper.on('qunar.prices.loaded', function() {	
+	this.waitUntilVisible(SEL.FULL_PRICE_BUTTON, function then() {
+		this.emit('qunar.button.fullPrice.loaded');		
 	});
 });
 
-casper.on('qunar.button.totalPrice.loaded', function() {
-	this.capture('screenshots/qunar.button.totalPrice.loaded.png');
-
-	this.click('#filter_showAllprice_yes');    
-	this.click('.btn_book');
-	this.waitWhileVisible('.qvt_loadding', function then() {
+casper.on('qunar.button.fullPrice.loaded', function() {
+	this.click(SEL.FULL_PRICE_BUTTON);    
+	this.click(SEL.BOOKING_BUTTON);
+	this.waitUntilVisible(SEL.FLIGHT_LEGS, function then() {
 		this.emit('qunar.prices.vendors.loaded');
 	});
 });
 
-casper.on('qunar.prices.vendors.loaded', function() {
-	this.capture('screenshots/qunar.prices.vendors.loaded.png');
-	var vendors = getLowestPricedVendors();
-   	debugger
+casper.on('qunar.prices.vendors.loaded', function() {	
+	this.each(getLowestPricedVendors(), function(self, vendor) {
+		self.echo(vendor.price);
+		self.echo(vendor.bookingButton);
+	});
 });
 
-var url = 'http://flight.qunar.com/site/interroundtrip_compare.htm?fromCity=%E4%B8%8A%E6%B5%B7&toCity=%E6%9B%BC%E8%B0%B7&fromDate=2015-12-15&toDate=2015-12-22&fromCode=SHA&toCode=BKK&from=qunarindex&lowestPrice=null&isInter=true&favoriteKey=&showTotalPr=null';
+var url = 'http://flight.qunar.com/site/interroundtrip_compare.htm?fromCity=%E4%B8%8A%E6%B5%B7&toCity=%E6%9B%BC%E8%B0%B7&fromDate=2015-10-01&toDate=2015-10-06&fromCode=SHA&toCode=BKK&from=qunarindex&lowestPrice=null&isInter=true&favoriteKey=&showTotalPr=null';
 
 casper.start(url, function then() {
 	this.emit('qunar.loaded');
@@ -65,16 +71,14 @@ casper.start(url, function then() {
 casper.run();
 
 var getLowestPricedVendors = function() {
-	return casper.evaluate(function() {
-		//return $('.b_spc_list').has('.low_pr').map(function() {
+	return casper.evaluate(function(SEL) {
+		return $(SEL.FLIGHT_LEGS).has(SEL.LOWEST_PRICE).map(function() {
 			return {
 				price: 100,
 				bookingButton: 'button'
 			};
-		//}).get();
-		//return 3;
-	});
-	//return 3;
+		}).get();
+	}, SEL);
 };
 
 /*var getLowestPrice = function(casper) {
