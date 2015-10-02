e5e65d974a7d7eefa07d1515ac48a304e78eceb9
l1on <iluvzhouying@gmail.com>
2015-10-02 20:06:31 +0800

    Capture all the urls of the new windows(vender booking pages)

--- qunar.js
+++ qunar.js
@@ -1,6 +1,6 @@
 var SEL = {
 	FLIGHT_LEGS: '.b_spc_list',
-	LOWEST_PRICE: '.low_pr',
+	LOWEST_PRICE: '.prc.low_pr',
 	FULL_PRICE_BUTTON: '#filter_showAllprice_yes',
 	BOOKING_BUTTON: '.btn_book',
 	PRICE_LOADING_PROGRESS_BAR: '.m-loader-inner[style*=overflow]',
@@ -28,6 +28,8 @@ var casper = require('casper').create({
     },
 });
 
+var numLoadingBookingPopups = 0;
+
 casper.each(EVENTS_TO_SCREEN_CAPTURE, function(self, event) {
 	self.on(event, function() {
 		this.capture('screenshots/' + event + '.png');
@@ -37,6 +39,18 @@ casper.each(EVENTS_TO_SCREEN_CAPTURE, function(self, event) {
 casper.on('waitFor.timeout', function(timeout, details) {
 	var incident = details.selector ? details.selector : details.testFx.name;
 	this.capture('screenshots/qunar.timeout.' + incident + '.png');
+});
+
+casper.on('popup.created', function() {
+	numLoadingBookingPopups++;
+});
+
+casper.on('popup.loaded', function(page) {
+	this.echo(page.url);
+
+	if ((--numLoadingBookingPopups) === 0) {
+		this.exit();
+	}
 });
 
 casper.on('qunar.loaded', function() {
@@ -58,19 +72,19 @@ casper.on('qunar.prices.vendors.loaded', function() {
 	var vendors = getLowestPricedVendors();
 	this.each(vendors, function(self, vendor) {
 		self.echo(vendor.price);
-		self.echo(vendor.bookingButtonId);
+		self.click('#' + vendor.bookingButtonId);
 	});
-
-	this.click('#' + vendors[0].bookingButtonId);
 });
 
-var url = 'http://flight.qunar.com/site/interroundtrip_compare.htm?fromCity=%E4%B8%8A%E6%B5%B7&toCity=%E6%9B%BC%E8%B0%B7&fromDate=2015-10-01&toDate=2015-10-06&fromCode=SHA&toCode=BKK&from=qunarindex&lowestPrice=null&isInter=true&favoriteKey=&showTotalPr=null';
+var url = 'http://flight.qunar.com/site/interroundtrip_compare.htm?fromCity=%E4%B8%8A%E6%B5%B7&toCity=%E6%9B%BC%E8%B0%B7&fromDate=2015-10-14&toDate=2015-10-21&fromCode=SHA&toCode=BKK&from=fi_re_search&lowestPrice=null&isInter=true&favoriteKey=&showTotalPr=null';
 
 casper.start(url, function then() {
 	this.emit('qunar.loaded');
 });
 
-casper.run();
+casper.run(function() {
+	// this function is deliberately empty so that casper will keep running until an explicit call to exit()
+});
 
 var getLowestPricedVendors = function() {
 	return casper.evaluate(function(SEL) {
